f10cd788490a2bb541b40b67363563c5707e5c85
pgiblock <pgiblock@cisco.com>
2016-03-04 13:48:31 -0500

    Add optional argument for max size
    `./hugemouse 1` to reset to 1.0 scale

--- hugemouse.c
+++ hugemouse.c
@@ -1,18 +1,29 @@
+#include <errno.h>
 #include <unistd.h>
 #include "altermouse.h"
 
 int
-main(void)
+main(int argc, char *argv[])
 {
     CGSConnectionID conn;
-    int i;
+    long i, tmp_size, size;
+
+    size = 256;
+
+    if (argc == 2) {
+        errno = 0;
+        tmp_size = strtol(argv[1], NULL, 10);
+        if (errno == 0) {
+            size = tmp_size;
+        }
+    }
 
     conn = CGSMainConnectionID();
 
     // Just keep trying larger sizes. This way the largest possible one will
     // "stick" before we start requesting sizes out of bounds.
-    for (i=0; i<256; i++) {
-	CGSSetCursorScale(conn, (float)i);
+    for (i=0; i<=size; i++) {
+        CGSSetCursorScale(conn, (float)i);
     }
 
     exit(EXIT_SUCCESS);
