b6aa8a445157405f9e4d193d3fb7e6638e0d1621
pgiblock <pgiblock@cisco.com>
2016-09-27 13:34:43 -0400

    Style

--- altermouse.h
+++ altermouse.h
@@ -1,3 +1,6 @@
+#ifndef ALTERMOUSE_H__
+#define ALTERMOUSE_H__
+
 #include <Carbon/Carbon.h>
 typedef int CGSConnectionID;
 CG_EXTERN CGSConnectionID CGSMainConnectionID(void);
@@ -5,3 +8,5 @@ CG_EXTERN CGError CGSGetCursorScale(CGSConnectionID cid, float *outScale);
 CG_EXTERN CGError CGSSetCursorScale(CGSConnectionID cid, float scale);
 CG_EXTERN CGError CGSForceWaitCursorActive(CGSConnectionID cid, bool showWaitCursor);
 CG_EXTERN int CGSCurrentCursorSeed(void);
+
+#endif // ALTERMOUSE_H__
--- hugemouse.c
+++ hugemouse.c
@@ -20,8 +20,10 @@ main(int argc, char *argv[])
 
     conn = CGSMainConnectionID();
 
-    // Just keep trying larger sizes. This way the largest possible one will
-    // "stick" before we start requesting sizes out of bounds.
+    /*
+     * Just keep trying larger sizes. This way the largest possible one will
+     * "stick" before we start requesting sizes out of bounds.
+     */
     for (i=0; i<=size; i++) {
         CGSSetCursorScale(conn, (float)i);
     }
